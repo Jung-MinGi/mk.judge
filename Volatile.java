9fd4564587ea13bbbb263436142946995cb3582c
MinGi-Jung <118701129+Jung-MinGi@users.noreply.github.com>
2024-07-02 16:36:55 +0900

    Update Volatile.java

--- Volatile.java
+++ Volatile.java
@@ -2,7 +2,13 @@ package concurrent;
 
 public class VolatileTest  extends Thread{
       //아래처럼 변수를 선언해버리면 cpu가 flag값을 캐시메모리에서 가져오므로 22번쨰 줄에서 flag값을 변경해도 적용되지 않아 무한루프가 돈다  
-//    boolean flag=true;
+//boolean flag=true;
+
+   //volatile는 읽기,쓰기에대한 동기화를 보장
+   //하나의 변수를 여러스레드가 참조할 때 사용하는 키워드
+   //volatile가 붙어있는 변수에 스레드가 접근하면 cpu cache에 접근하는것이 아니라 메인메모리에서 작업을 수행함
+      
+   
    volatile boolean flag=true;
 
     @Override
